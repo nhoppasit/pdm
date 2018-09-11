e66a12f86f8bc0221b96b36147f093f092633bbc
Nhoppasit <dev.theprogram@gmail.com>
2018-09-11 20:34:40 +0700

    เพิ่มโครงงานด้าน ห้องควบคุม

---
+++ src/PDM2019/PDM2019.RoomControl/Class/PickingDetail.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDM2019.RoomControl.Class
+{
+    public class PickingDetail
+    {
+        //GridViewColumn Header = "ลำดับที่" Width="120" DisplayMemberBinding="{Binding NO}" />
+        //<GridViewColumn Header = "รหัสสินค้า" Width="150" DisplayMemberBinding="{Binding ITEM_CODE}" />
+        //<GridViewColumn Header = "รายการ" Width="100" DisplayMemberBinding="{Binding ITEM_LABEL}" />
+        //<GridViewColumn Header = "ขนาด" Width="70" DisplayMemberBinding="{Binding ITEM_SIZE}" />
+        //<GridViewColumn Header = "บรรจุ" Width="100" DisplayMemberBinding="{Binding PACKAGES_IN_BASKET}" />
+        //<GridViewColumn Header = "น้ำหนักรวม" Width="100" DisplayMemberBinding="{Binding WEIGHT}" />
+        //<GridViewColumn Header = "จำนวนเบิก" Width="70" DisplayMemberBinding="{Binding TAKE_COUNT}" />
+        //<GridViewColumn Header = "ตระกร้าเต็ม" Width="70" DisplayMemberBinding="{Binding FULL_FILLS}" />
+        //<GridViewColumn Header = "เศษ" Width="100" DisplayMemberBinding="{Binding RESIDUAL}" />
+        //<GridViewColumn Header = "จัดตะกร้าเต็ม" Width="100" DisplayMemberBinding="{Binding FULL_FILL_PICKS}" />
+        //<GridViewColumn Header = "จัดตะกร้าเศษ" Width="100" DisplayMemberBinding="{Binding RESIDUAL_PICKS}" />
+        //<GridViewColumn Header = "Progress" Width="100">
+        //    <GridViewColumn.CellTemplate>
+        //        <DataTemplate>
+        //            <ProgressBar Width = "180" Height="20" Margin="0" Minimum="0" Maximum="100" Foreground="Red" Value="{Binding PICK_PERCENTS}"/>
+        //        </DataTemplate>
+        //    </GridViewColumn.CellTemplate>
+        //</GridViewColumn>
+        //<GridViewColumn Header = "%" Width="30" DisplayMemberBinding="{Binding PICK_PERCENTS}" />
+        //<GridViewColumn Header = "Date-time" Width="100" DisplayMemberBinding="{Binding LAST_ACTIVITY_DATETIME}" />
+        //<GridViewColumn Header = "Activity" Width="200" DisplayMemberBinding="{Binding LAST_ACTIVITY}" />
+
+        public int NO { get; set; }
+        public string ITEM_CODE { get; set; }
+        public string ITEM_LABEL { get; set; }
+        public int ITEM_SIZE { get; set; }
+        public int PACKAGES_IN_BASKET { get; set; }
+        public decimal WEIGHT { get; set; }
+        public int TAKE_COUNT { get; set; }
+        public int FULL_FILLS { get; set; }
+        public int RESIDUAL { get; set; }
+        public int FULL_FILL_PICKS { get; set; }
+        public int RESIDUAL_PICKS { get; set; }
+        public int PICK_PERCENTS { get; set; }
+        public DateTime? LAST_ACTIVITY_DATETIME { get; set; }
+        public string LAST_ACTIVITY { get; set; }
+
+        public PickingDetail(int no, string code, string label, int size, int packagesInBasket, decimal weight, int take, int pickFull, int pickResidual, DateTime? actDateTime, string activity)
+        {
+            this.NO = no;
+            this.ITEM_CODE = code;
+            this.ITEM_LABEL = label;
+            this.ITEM_SIZE = size;
+            this.PACKAGES_IN_BASKET = packagesInBasket;
+            this.WEIGHT = weight;
+            this.TAKE_COUNT = take;
+            this.FULL_FILL_PICKS = pickFull;
+            this.RESIDUAL_PICKS = pickResidual;
+            this.LAST_ACTIVITY_DATETIME = actDateTime;
+            this.LAST_ACTIVITY = activity;
+
+            this.RESIDUAL = (int)((double)take % (double)packagesInBasket);
+            this.FULL_FILLS = (int)((double)(take - this.RESIDUAL) / (double)packagesInBasket);
+            this.PICK_PERCENTS = (int)(((double)this.FULL_FILL_PICKS * (double)this.PACKAGES_IN_BASKET + (double)this.RESIDUAL_PICKS) / (double)this.TAKE_COUNT * 100);
+        }
+
+    }
+}
--- src/PDM2019/PDM2019.RoomControl/MainWindow.xaml.cs
+++ src/PDM2019/PDM2019.RoomControl/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Controls.Ribbon;
+using PDM2019.Office.Class;
+using PDM2019.RoomControl.Class;
 
 namespace PDM2019.RoomControl
 {
@@ -24,43 +26,83 @@ namespace PDM2019.RoomControl
         public MainWindow()
         {
             InitializeComponent();
+
+            list.SelectionChanged += List_SelectionChanged;
+            mnuDetailRefresh.Click += MnuDetailRefresh_Click;
+
+            DefineSampleData();
+        }
+
+        private void MnuDetailRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                PO_Summary po = (PO_Summary)(this.list.SelectedItem);
+                List<PickingDetail> pickings = new List<PickingDetail>();
+                Random rnd = new Random();
+                for (int i = 0; i < po.PRODUCT_COUNT; i++)
+                {
+                    PickingDetail pick = new PickingDetail(i + 1, "23058403", "ฟุตลองสไปซี่ (SCB-12)", 445, 20, 16.2m, 36 * (i + 1), rnd.Next(5), rnd.Next(20), DateTime.Now, "นาย ก อ่านบาร์โค้ด");
+                    pickings.Add(pick);
+                }
+                lvDetail.ItemsSource = null;
+                lvDetail.ItemsSource = pickings;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void List_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                PO_Summary po = (PO_Summary)e.AddedItems[0];
+                List<PickingDetail> pickings = new List<PickingDetail>();
+                Random rnd = new Random();
+                for (int i = 0; i < po.PRODUCT_COUNT; i++)
+                {
+                    PickingDetail pick = new PickingDetail(i + 1, "23058403", "ฟุตลองสไปซี่ (SCB-12)", 445, 20, 16.2m, 36, rnd.Next(5), rnd.Next(20), DateTime.Now, "นาย ก อ่านบาร์โค้ด");
+                    pickings.Add(pick);
+                }
+                lvDetail.ItemsSource = null;
+                lvDetail.ItemsSource = pickings;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void DefineSampleData()
         {
-            //PO_Summary item;
-            //List<PO_Summary> items = new List<PO_Summary>();
+            PO_Summary item;
+            List<PO_Summary> items = new List<PO_Summary>();
 
-            //item = new PO_Summary(lineCode: "NNSS09", poName: "สุวรรณภูมิ ไส้กรอก PO9", productCount: 41, weight: 932, totalTake: 5234, fullFIll: 100, residualBaskets: 30, residual: 800, currentPicks: 1000, lastActDateTime: DateTime.Now, lastActivity: "นาย ก, ฟุตลองสไปซี่ (SCB-12), 20/36");
-            //items.Add(item);
-            //PlaceProgressBar(MyMap, item.PICK_PERCENTS, new Location(18.796143, 98.979263), Colors.Green);
-            //item = new PO_Summary(lineCode: "NNSS10", poName: "สุวรรณภูมิ ไส้กรอก P10", productCount: 41, weight: 934, totalTake: 2334, fullFIll: 200, residualBaskets: 31, residual: 801, currentPicks: 1000, lastActDateTime: DateTime.Now, lastActivity: "นาย ข, ฟุตลองสไปซี่ (SCB-12), เสร็จแล้ว");
-            //items.Add(item);
-            //PlaceProgressBar(MyMap, item.PICK_PERCENTS, new Location(13.69269, 100.750465), Colors.Green);
-            //item = new PO_Summary(lineCode: "NNSS11", poName: "สุวรรณภูมิ ไส้กรอก P11", productCount: 41, weight: 935, totalTake: 3434, fullFIll: 300, residualBaskets: 32, residual: 802, currentPicks: 1000, lastActDateTime: DateTime.Now, lastActivity: "นาย ค, ฟุตลองสไปซี่ (SCB-12), 40/106");
-            //items.Add(item);
-            //PlaceProgressBar(MyMap, item.PICK_PERCENTS, new Location(9.14011, 99.33311), Colors.Green);
-            //item = new PO_Summary(lineCode: "NNSS12", poName: "สุวรรณภูมิ ไส้กรอก P12", productCount: 41, weight: 936, totalTake: 4534, fullFIll: 400, residualBaskets: 33, residual: 804, currentPicks: 1000, lastActDateTime: DateTime.Now, lastActivity: "นาย ง, ฟุตลองสไปซี่ (SCB-12), 60/306");
-            //items.Add(item);
-            //PlaceProgressBar(MyMap, item.PICK_PERCENTS, new Location(7.900544, 98.985268), Colors.Green);
-            //item = new PO_Summary(lineCode: "NNSS13", poName: "สุวรรณภูมิ ไส้กรอก P13", productCount: 41, weight: 937, totalTake: 5634, fullFIll: 500, residualBaskets: 34, residual: 805, currentPicks: 1000, lastActDateTime: null, lastActivity: "");
-            //items.Add(item);
-            //PlaceProgressBar(MyMap, item.PICK_PERCENTS, new Location(13.03887, 101.490104), Colors.Green);
-            //item = new PO_Summary(lineCode: "NNSS14", poName: "สุวรรณภูมิ ไส้กรอก P14", productCount: 41, weight: 938, totalTake: 6734, fullFIll: 600, residualBaskets: 35, residual: 806, currentPicks: 1000, lastActDateTime: null, lastActivity: "");
-            //items.Add(item);
-            //PlaceProgressBar(MyMap, item.PICK_PERCENTS, new Location(13.922104600, 101.577931500), Colors.Green);
-            //item = new PO_Summary(lineCode: "NNSS15", poName: "สุวรรณภูมิ ไส้กรอก P15", productCount: 41, weight: 939, totalTake: 7834, fullFIll: 700, residualBaskets: 36, residual: 807, currentPicks: 1000, lastActDateTime: null, lastActivity: "");
-            //items.Add(item);
-            //PlaceProgressBar(MyMap, item.PICK_PERCENTS, new Location(10.72167, 99.26259), Colors.Green);
-            //item = new PO_Summary(lineCode: "NNSS16", poName: "สุวรรณภูมิ ไส้กรอก P16", productCount: 41, weight: 940, totalTake: 8934, fullFIll: 800, residualBaskets: 37, residual: 808, currentPicks: 1000, lastActDateTime: null, lastActivity: "");
-            //items.Add(item);
-            //PlaceProgressBar(MyMap, item.PICK_PERCENTS, new Location(12.671163982, 101.273998904), Colors.Green);
+            item = new PO_Summary(lineCode: "NNSS09", poName: "สุวรรณภูมิ ไส้กรอก PO9", productCount: 41, weight: 932, totalTake: 5234, fullFIll: 100, residualBaskets: 30, residual: 800, currentPicks: 1000, lastActDateTime: DateTime.Now, lastActivity: "นาย ก, ฟุตลองสไปซี่ (SCB-12), 20/36");
+            items.Add(item);
+            item = new PO_Summary(lineCode: "NNSS10", poName: "สุวรรณภูมิ ไส้กรอก P10", productCount: 30, weight: 934, totalTake: 2334, fullFIll: 200, residualBaskets: 31, residual: 801, currentPicks: 1000, lastActDateTime: DateTime.Now, lastActivity: "นาย ข, ฟุตลองสไปซี่ (SCB-12), เสร็จแล้ว");
+            items.Add(item);
+            item = new PO_Summary(lineCode: "NNSS11", poName: "สุวรรณภูมิ ไส้กรอก P11", productCount: 33, weight: 935, totalTake: 3434, fullFIll: 300, residualBaskets: 32, residual: 802, currentPicks: 1000, lastActDateTime: DateTime.Now, lastActivity: "นาย ค, ฟุตลองสไปซี่ (SCB-12), 40/106");
+            items.Add(item);
+            item = new PO_Summary(lineCode: "NNSS12", poName: "สุวรรณภูมิ ไส้กรอก P12", productCount: 20, weight: 936, totalTake: 4534, fullFIll: 400, residualBaskets: 33, residual: 804, currentPicks: 1000, lastActDateTime: DateTime.Now, lastActivity: "นาย ง, ฟุตลองสไปซี่ (SCB-12), 60/306");
+            items.Add(item);
+            item = new PO_Summary(lineCode: "NNSS13", poName: "สุวรรณภูมิ ไส้กรอก P13", productCount: 40, weight: 937, totalTake: 5634, fullFIll: 500, residualBaskets: 34, residual: 805, currentPicks: 1000, lastActDateTime: null, lastActivity: "");
+            items.Add(item);
+            item = new PO_Summary(lineCode: "NNSS14", poName: "สุวรรณภูมิ ไส้กรอก P14", productCount: 35, weight: 938, totalTake: 6734, fullFIll: 600, residualBaskets: 35, residual: 806, currentPicks: 1000, lastActDateTime: null, lastActivity: "");
+            items.Add(item);
+            item = new PO_Summary(lineCode: "NNSS15", poName: "สุวรรณภูมิ ไส้กรอก P15", productCount: 27, weight: 939, totalTake: 7834, fullFIll: 700, residualBaskets: 36, residual: 807, currentPicks: 1000, lastActDateTime: null, lastActivity: "");
+            items.Add(item);
+            item = new PO_Summary(lineCode: "NNSS16", poName: "สุวรรณภูมิ ไส้กรอก P16", productCount: 17, weight: 940, totalTake: 8934, fullFIll: 800, residualBaskets: 37, residual: 808, currentPicks: 1000, lastActDateTime: null, lastActivity: "");
+            items.Add(item);
 
-            //list.ItemsSource = items;
+            list.ItemsSource = items;
 
-            //Location textLocation = MyMap.Center;
-            //string text = "70%";
-            //PlaceText(MyMap, text, textLocation, Colors.Yellow, 16.0);
         }
+
+
+
+
     }
 }
